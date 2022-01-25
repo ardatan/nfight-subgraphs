b8d94897876d383d63ccdbff33c323f93f70b155
Sam Bauch <sbauch@gmail.com>
2022-01-25 16:10:54 -0500

    add apollo server

---
+++ nfight-mesh/server.js
@@ -0,0 +1,19 @@
+const { ApolloServer } = require('apollo-server');
+
+module.exports = async ({ getBuiltMesh, documents, logger }) => {
+  const { schema } = await getBuiltMesh();
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req }) => req,
+    playground: {
+      tabs: documents.map(({ location, rawSDL }) => ({
+        name: location,
+        endpoint: '/graphql',
+        query: rawSDL,
+      })),
+    },
+  });
+
+  const { url } = await apolloServer.listen(4000);
+  logger.info(`🚀 Server ready at ${url}`);
+};
