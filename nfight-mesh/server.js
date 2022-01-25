277710a165c37cad8555dec22b39865fe968bc5b
Sam Bauch <sbauch@gmail.com>
2022-01-25 16:33:48 -0500

    port

--- nfight-mesh/server.js
+++ nfight-mesh/server.js
@@ -1,10 +1,16 @@
 const { ApolloServer } = require('apollo-server');
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground
+} = require("apollo-server-core");
 
 module.exports = async ({ getBuiltMesh, documents, logger }) => {
   const { schema } = await getBuiltMesh();
   const apolloServer = new ApolloServer({
     schema,
     context: ({ req }) => req,
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground
+    ],
     playground: {
       tabs: documents.map(({ location, rawSDL }) => ({
         name: location,
